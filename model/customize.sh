77d63a920c14aa801330c3e70c8e40dd285e7bd3
947409161 <947409161@qq.com>
2026-06-30 19:38:31 +0800

    上传基础代码

---
+++ model/customize.sh
@@ -0,0 +1,15 @@
+SKIPUNZIP=1
+MIN_KSUD_VERSION=32000
+MODDIR=${0%/*}
+if [ "$BOOTMODE" ] && [ "$KSU" ]; then
+  ui_print "- Installing from KernelSU app"
+  ui_print "- KernelSU version: $KSU_KERNEL_VER_CODE (kernel) + $KSU_VER_CODE (ksud)"
+  if ! [ "$KSU_KERNEL_VER_CODE" ] || [ "$KSU_KERNEL_VER_CODE" -lt "$MIN_KSU_VERSION" ]; then
+    ui_print "*********************************************************"
+    ui_print "! KernelSU version is too old!"
+    ui_print "! Please update KernelSU to latest version"
+    abort    "*********************************************************"
+  fi
+exit
+chmod 644 "$MODDIR"
+chmod 755 "$MODDIR/lkm"
