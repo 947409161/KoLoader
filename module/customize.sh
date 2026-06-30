2ce9bd004211c5ad3a40448ad08489d31aa45dc0
生于生时 亡于亡刻 <127004703+Tools-cx-app@users.noreply.github.com>
2026-06-30 20:18:34 +0800

    Merge pull request #1 from Tools-cx-app/main
    perf: use ko-loader to load ko

--- module/customize.sh
+++ module/customize.sh
@@ -1,3 +1,20 @@
-SKIPUNZIP=1
-chmod 644 "$MODDIR"
+#!/system/bin/sh
+
+case "$ARCH" in
+arm64)
+  ui_print "- Selected architecture: arm64-v8a"
+  ARCH_BINARY="arm64-v8a"
+  ;;
+arm)
+  ui_print "- Selected architecture: armeabi-v7a"
+  ARCH_BINARY="armeabi-v7a"
+  ;;
+*)
+  abort "! Unsupported platform: $ABI"
+  ;;
+esac
+
+chmod 755 "$MODPATH/bin/$ARCH_BINARY" -R || abort "! Failed to set permissions"
+ln -s "./bin/$ARCH_BINARY/ko-loader" "$MODPATH/loader" || abort "! Failed to create symlink"
 chmod 755 "$MODDIR/lkm"
+
